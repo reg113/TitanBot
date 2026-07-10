c59078bff3810821d4e255a5c3e77678119e76f2
reg113 <march7122@tutamail.com>
2026-07-10 15:28:45 +0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -170,7 +170,7 @@ export const botConfig = {
     startingBalance: 0,
 
     // Maximum bank amount before upgrades (if upgrades are used).
-    baseBankCapacity: 100000,
+    baseBankCapacity: 5000,
 
     // Daily reward amount.
     dailyAmount: 100,
